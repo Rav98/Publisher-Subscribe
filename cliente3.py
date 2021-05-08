bf097d1e79565f407fd413b4dec2afcd17e7c33b
Rafael Antunes Vieira <rafaelantunesvieira@outlook.com.br>
2021-05-08 12:51:10 -0300

    Criação da parte de escolha do Subscribe

--- cliente3.py
+++ cliente3.py
@@ -1,18 +1,35 @@
-# Cliente 3 
+# Cliente 3
 
 #   Importando o socket
 import socket
 
-#   Definindo meu Host e porta 
-HOST='localhost'
-PORT=5003
+#   Definindo meu Host e porta
+HOST = 'localhost'
+PORT = 5003
 
 #   Criando o objeto socket
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #   Pedindo a conexao ao servidor
-s.connect((HOST,PORT))
+s.connect((HOST, PORT))
 
-#   Recebendo resposta da comunicacao e printando 
-data=s.recv(1024)
-print(data.decode())
+#   Recebendo a lista de Publichers
+listaPub1 = s.recv(1024)
+listaPub2 = s.recv(1024)
+listaPub3 = s.recv(1024)
+print('Publishers cadastrados:')
+print(listaPub1.decode())
+print(listaPub2.decode())
+print(listaPub3.decode())
+
+#   perguntar oa usuario qual publisher ele quer se inscrever
+print('Qual Publisher Voce que se inscrever?')
+resposta = input()
+
+#   envia a resposta
+print('Resposta enviada: ', resposta)
+s.sendall(str.encode(resposta))
+
+#   Recebendo resposta da comunicacao
+data = s.recv(1024)
+print('Resposta recebida:', data.decode())
--- servidor.py
+++ servidor.py
@@ -10,6 +10,9 @@ PORT1 = 5001
 PORT2 = 5002
 PORT3 = 5003
 
+# Publichers cadastrados
+listaPub = ['Alef', 'Flavio', 'Rafael']
+
 # Criando o objeto socket
 pc1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 pc2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,37 +35,53 @@ print('Aguardando conexao do PC3')
 
 #   Retorno do "aceitamento" da conexão
 connexao1, endereco1 = pc1.accept()
-print('Conectado em: ', endereco1)
+print('Conectado com: ', endereco1)
 
 connexao2, endereco2 = pc2.accept()
-print('Conectado em: ', endereco2)
+print('Conectado com: ', endereco2)
 
 connexao3, endereco3 = pc3.accept()
-print('Conectado em: ', endereco3)
+print('Conectado com: ', endereco3)
 
-#   Conecao feita, hora de configura o PubSub ----------------
+# Envia a lista de Publishers:
+for n in listaPub:
+    connexao1.sendall(str.encode(n))
+    connexao2.sendall(str.encode(n))
+    connexao3.sendall(str.encode(n))
+
+# Recebe Publisher escolhido pelos subscribes
+listaRespSub1 = connexao1.recv(1024)
+listaRespSub2 = connexao2.recv(1024)
+listaRespSub3 = connexao3.recv(1024)
+
+# imprime a escolha dos subscribes
+print(listaRespSub1.decode())
+print(listaRespSub2.decode())
+print(listaRespSub3.decode())
 
 #   Função que envia dados para o pc1
+
 def pc1(data):
     connexao1.sendall(str.encode(data))
 
-#   Função que envia dados para o pc2
+
 def pc2(data):
     connexao2.sendall(str.encode(data))
 
-#   Função que envia dados para o pc2
+
 def pc3(data):
     connexao3.sendall(str.encode(data))
 
-#   Função responsavel por inscrever os PCs nos grupos 
-pub.subscribe(pc1, 'Rafael')
-pub.subscribe(pc2, 'Flavio')
-pub.subscribe(pc3, 'Alef')
+
+#   Função responsavel por inscrever os PCs nos grupos
+pub.subscribe(pc1, listaRespSub1.decode())
+pub.subscribe(pc2, listaRespSub2.decode())
+pub.subscribe(pc3, listaRespSub3.decode())
 
 #   Função responsavel por mandar as mensagens para determinado grupo inscrito
-pub.sendMessage('Rafael', data='SERVIDOR MANDOU PARA O PC1')
-pub.sendMessage('Flavio', data='SERVIDOR MANDOU PARA O PC2')
-pub.sendMessage('Alef', data='SERVIDOR MANDOU PARA O PC3')  
+pub.sendMessage(listaRespSub1.decode(), data='SERVIDOR MANDOU PARA O PC1')
+pub.sendMessage(listaRespSub2.decode(), data='SERVIDOR MANDOU PARA O PC2')
+pub.sendMessage(listaRespSub3.decode(), data='SERVIDOR MANDOU PARA O PC3')
 
 print('Mensagem enviada aos Clientes!')
 
