7c1a8d3ba1d89d4b5e2ecc2877f6c00c8a125fcc
Rafael Antunes Vieira <rafaelantunesvieira@outlook.com.br>
2021-05-10 14:11:22 -0300

    Finalizando correções

--- cliente3.py
+++ cliente3.py
@@ -1,6 +1,6 @@
-# Cliente 3
+# Subscribe 3
 
-#   Importando o socket
+#   Importando a biblioteca do Socket
 import socket
 
 #   Definindo meu Host e porta
@@ -12,13 +12,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print('Conectando.......\n')
 
-#   Pedindo a conexao ao servidor
+#   Pedindo a conexao ao Broker
 s.connect((HOST, PORT))
 
 print('Conexão concluida!\n')
 
 #   Lista de inscrições do usuario
-inscricoes1 = ['']
+inscricoes = ['']
 
 #   Contador de quantas inscrições o Inscrito tem
 global contInscri
@@ -32,13 +32,13 @@ def Inscrição(contInscri):
     listaPub2 = s.recv(1024)
     listaPub3 = s.recv(1024)
 
-    #   Imprimindo a lista:
+    #   Imprimindo a lista de Publichers para o Subscribe escolher:
     print('\nPublishers cadastrados:')
     print('->', listaPub1.decode())
     print('->', listaPub2.decode())
     print('->', listaPub3.decode())
 
-    #   Perguntar oa usuario qual publisher ele quer se inscrever
+    #   Perguntar oa Subscribe qual publisher ele quer se inscrever
     print('Qual Publisher Voce que se inscrever?')
     resposta = input()
 
@@ -46,6 +46,8 @@ def Inscrição(contInscri):
     print('\nInscrição concluida em: ', resposta)
     s.sendall(str.encode(resposta))
     return contInscri
+
+#   Função responsavel por receber a mensagem do Broker
 
 
 def RecebeMensagem(contInscri):
@@ -56,18 +58,20 @@ def RecebeMensagem(contInscri):
         data = s.recv(1024)
         print('\nMensagem recebida: ', data.decode(), '\n')
 
+#   Função responsavel por enviar a requisição de desinscrição do publisher
+
 
 def Desinscrever(contInscri):
     contInscri = contInscri-1
-    numInscricoes1 = s.recv(1024)
+    numinscricoes = s.recv(1024)
     cont = 0
-    while cont < (int(numInscricoes1.decode())):
-        inscricoes1.append(s.recv(1024))
+    while cont < (int(numinscricoes.decode())):
+        inscricoes.append(s.recv(1024))
         cont += 1
 
     #   Imprimindo a lista:
     print('\nInscritos nos Publichers:')
-    for n in inscricoes1:
+    for n in inscricoes:
         print(n)
 
     #   Perguntar oa usuario qual publisher ele quer se desinscrever
@@ -78,30 +82,30 @@ def Desinscrever(contInscri):
     print('\nDesinscrito em: ', resposta, '\n')
     s.sendall(str.encode(resposta))
 
-    #   Limpa a lista aqui para receber uma nova lista atualizada do servidor
-    inscricoes1.clear()
+    #   Limpa a lista aqui para receber uma nova lista atualizada do Broker
+    inscricoes.clear()
     return contInscri
 
 
+# Rotina de execução do Subscribe
 while True:
 
-    #   Lendo opção que o cliente quer fazer
+    #   Lendo opção que o Subscribe deseja fazer:
     print('\n----Menu----\n')
     print('Digite 1 para receber mensagens')
     print('Digite 2 para se inscrever')
     print('Digite 3 para se desinscrever')
     print('Digite 4 para sair')
     opcao = input()
-
+    #   Comunica o Broker da intenção do Subscribe
     s.sendall(str.encode(opcao))
-
+    # Executa as funç~eos de acordo com o escolhido pelo Subscribe
     if (opcao == '1'):
-        RecebeMensagem(contInscri)  
+        RecebeMensagem(contInscri)
     elif (opcao == '2'):
         contInscri = Inscrição(contInscri)
     elif (opcao == '3'):
         contInscri = Desinscrever(contInscri)
-        
     else:
         s.close()
         break
--- servidor.py
+++ servidor.py
@@ -1,19 +1,19 @@
-#   Importando o socket
+#   Importando a biblioteca do Socket
 import socket
 
 #   Importando a biblioteca do PyPubSub
 from pubsub import pub
 
-#   Definindo meu Host e porta
+#   Definindo meu Host e Porta
 HOST = 'localhost'
 PORT1 = 5001
 PORT2 = 5002
 PORT3 = 5003
 
-#   Publichers cadastrados
+#   Lista de Publichers cadastrados no sistema
 listaPub = ['Alef', 'Flavio', 'Rafael']
 
-#   Lista para salvar as inscrições de cada PCs
+#   Lista para salvar as inscrições de cada Subscribe
 global listaDeinscricaoPC1
 listaDeinscricaoPC1 = []
 global listaDeinscricaoPC2
@@ -21,12 +21,12 @@ listaDeinscricaoPC2 = []
 global listaDeinscricaoPC3
 listaDeinscricaoPC3 = []
 
-#   Variaveis globais para salvar as respostas retornadas pelos PCs
+#   Variaveis globais para salvar as respostas retornadas pelos Subscribers
 global listaRespSub1
 global listaRespSub2
 global listaRespSub3
 
-#   Criando o objeto socket
+#   Criando o objeto socket para cada inscrito
 pc1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 pc2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 pc3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -46,7 +46,7 @@ print('Aguardando conexao do PC2')
 pc3.listen()
 print('Aguardando conexao do PC3')
 
-#   Retorno do "aceitamento" da conexão
+#   Retorno da confirmação da conexão pelo Subscribe
 connexao1, endereco1 = pc1.accept()
 print('Conectado com: ', endereco1)
 
@@ -56,7 +56,7 @@ print('Conectado com: ', endereco2)
 connexao3, endereco3 = pc3.accept()
 print('Conectado com: ', endereco3)
 
-#   Função que envia dados para o pc1
+#   Função que envia dados para os Subscribers inscritos
 
 
 def pc1(data):
@@ -71,34 +71,45 @@ def pc3(data):
     connexao3.sendall(str.encode(data))
 
 
+# Função responsavel por inscrever o usuario em algum Publisher
 def Inscrição(pc):
     if(pc == '1'):
         # Envia a lista de Publishers:
         for n in listaPub:
             connexao1.sendall(str.encode(n))
-        # Recebe Publisher escolhido pelos subscribes
+        # Recebe Publisher escolhido pelo usuario
         listaRespSub1 = connexao1.recv(1024)
         listaDeinscricaoPC1.append(listaRespSub1.decode())
-        # imprime a escolha dos subscribes
+        # imprime a escolha do usuario
         print('PC1 escolheu se inscrever em:', listaRespSub1.decode())
-        #   Função responsavel por inscrever os PCs nos grupos
+        #   Função responsavel por inscrever os Subscribe nos grupos
         pub.subscribe(pc1, listaRespSub1.decode())
 
     elif(pc == '2'):
+        # Envia a lista de Publishers:
         for n in listaPub:
             connexao2.sendall(str.encode(n))
+        # Recebe Publisher escolhido pelo usuario
         listaRespSub2 = connexao2.recv(1024)
         listaDeinscricaoPC2.append(listaRespSub2.decode())
+        # imprime a escolha do usuario
         print('PC2 escolheu se inscrever em:', listaRespSub2.decode())
+        #   Função responsavel por inscrever os Subscribe nos grupos
         pub.subscribe(pc2, listaRespSub2.decode())
 
     elif(pc == '3'):
+        # Envia a lista de Publishers:
         for n in listaPub:
             connexao3.sendall(str.encode(n))
+        # Recebe Publisher escolhido pelo usuario
         listaRespSub3 = connexao3.recv(1024)
         listaDeinscricaoPC3.append(listaRespSub3.decode())
+        # imprime a escolha do usuario
         print('PC3 escolheu se inscrever em:', listaRespSub3.decode())
+        #   Função responsavel por inscrever os Subscribe nos grupos
         pub.subscribe(pc3, listaRespSub3.decode())
+
+# Função responsavel por enviar a mensagem para cada Subscribe
 
 
 def EnviarMensagem():
@@ -112,7 +123,9 @@ def EnviarMensagem():
     print('\nEscreva uma mensagem para os Inscritos em:', listaPub[2])
     data3 = input()
     pub.sendMessage(listaPub[2], data=data3)
-    print('\nMensagem enviada aos Clientes!\n')
+    print('\nMensagem enviada aos Subscribers!\n')
+
+# Função responsavel por desinscrever o Subscribe
 
 
 def Desinscrever(pc):
@@ -142,8 +155,9 @@ def Desinscrever(pc):
         print('PC3 se desinscreveu de:', listaRespSub3.decode())
 
 
+# Rotina de execução do servidor.
 while True:
-    # ouvindo os clientes para saber se eles querem se inscrever
+    # ouvindo os Subscribers para saber se eles querem se inscrever ou desinscrever
     resposta1 = connexao1.recv(1024)
     if resposta1.decode() == '1':
         EnviarMensagem()
